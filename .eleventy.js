3da5ee7719ad683a326ab3ed6de1f165becae12b
pinage404 <pinage404@gmail.com>
2019-07-27 16:57:19 +0200

    feat: quick fail if TemplateData doesn't exist

--- .eleventy.js
+++ .eleventy.js
@@ -12,48 +12,51 @@ module.exports = {
   initArguments: {},
   configFunction: function(eleventyConfig, options = {}) {
     setImmediate(function() {
+      const TemplateData = require('@11ty/eleventy/src/TemplateData.js');
+      if (!TemplateData || !TemplateData.prototype) {
+        return;
+      }
+
       const path = require('path');
       const fs = require('fs');
       const lodashUniq = require('lodash.uniq');
       const jsyaml = require('js-yaml');
-      const TemplateData = require('@11ty/eleventy/src/TemplateData.js');
-      if (TemplateData.prototype) {
-        function getLocalData(original, tmplPath) {
-          return Promise.all([
-            original.apply(this, [tmplPath]),
-            this.getLocalDataPaths(tmplPath)
-          ]).then(function(args) {
-            const data = args[0];
-            const dataPaths = lodashUniq(
-              args[1].map(function(p) {
-                return p.substr(0, p.length - path.extname(p).length) + '.yaml';
+
+      function getLocalData(original, tmplPath) {
+        return Promise.all([
+          original.apply(this, [tmplPath]),
+          this.getLocalDataPaths(tmplPath)
+        ]).then(function(args) {
+          const data = args[0];
+          const dataPaths = lodashUniq(
+            args[1].map(function(p) {
+              return p.substr(0, p.length - path.extname(p).length) + '.yaml';
+            })
+          );
+          return Object.assign.apply(
+            null,
+            [data].concat(
+              dataPaths.map(function(fn) {
+                try {
+                  return jsyaml.load(fs.readFileSync(fn));
+                } catch (err) {
+                  return {};
+                }
               })
-            );
-            return Object.assign.apply(
-              null,
-              [data].concat(
-                dataPaths.map(function(fn) {
-                  try {
-                    return jsyaml.load(fs.readFileSync(fn));
-                  } catch (err) {
-                    return {};
-                  }
-                })
-              )
-            );
-          }, Promise.reject);
-        }
-        function getTemplateDataFileGlob(original) {
-          var suffix = this.config.jsDataFileSuffix;
-          return original.apply(this, arguments).then(function(arr) {
-            return arr.concat([
-              path.join(path.dirname(arr[0]), `*${suffix}.yaml`)
-            ]);
-          }, Promise.reject);
-        }
-        monkeypatch(TemplateData, getLocalData);
-        monkeypatch(TemplateData, getTemplateDataFileGlob);
+            )
+          );
+        }, Promise.reject);
       }
+      function getTemplateDataFileGlob(original) {
+        var suffix = this.config.jsDataFileSuffix;
+        return original.apply(this, arguments).then(function(arr) {
+          return arr.concat([
+            path.join(path.dirname(arr[0]), `*${suffix}.yaml`)
+          ]);
+        }, Promise.reject);
+      }
+      monkeypatch(TemplateData, getLocalData);
+      monkeypatch(TemplateData, getTemplateDataFileGlob);
     });
   }
 };
