161bb8985d9be12bc52ff6b88b3d1596c8c043ab
pinage404 <pinage404@gmail.com>
2019-07-27 16:42:26 +0200

    style: autoformat source

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
-function monkeypatch(cls,fn) {
+function monkeypatch(cls, fn) {
   const orig = cls.prototype[fn.name].__original || cls.prototype[fn.name];
   function wrapped() {
     return fn.bind(this, orig).apply(this, arguments);
   }
-  wrapped.__original = orig
+  wrapped.__original = orig;
 
   cls.prototype[fn.name] = wrapped;
 }
@@ -17,29 +17,38 @@ module.exports = {
       const lodashUniq = require('lodash.uniq');
       const jsyaml = require('js-yaml');
       const TemplateData = require('@11ty/eleventy/src/TemplateData.js');
-      if(TemplateData.prototype) {
+      if (TemplateData.prototype) {
         function getLocalData(original, tmplPath) {
           return Promise.all([
             original.apply(this, [tmplPath]),
             this.getLocalDataPaths(tmplPath)
-          ]) .then(function(args) {
+          ]).then(function(args) {
             const data = args[0];
-            const dataPaths = lodashUniq(args[1].map(function(p) {
-              return p.substr(0, p.length - path.extname(p).length) + '.yaml';
-            }));
-            return Object.assign.apply(null, [data].concat(dataPaths.map(function(fn) {
-              try {
-                return jsyaml.load(fs.readFileSync(fn));
-              } catch (err) {
-                return {};
-              }
-            })));
+            const dataPaths = lodashUniq(
+              args[1].map(function(p) {
+                return p.substr(0, p.length - path.extname(p).length) + '.yaml';
+              })
+            );
+            return Object.assign.apply(
+              null,
+              [data].concat(
+                dataPaths.map(function(fn) {
+                  try {
+                    return jsyaml.load(fs.readFileSync(fn));
+                  } catch (err) {
+                    return {};
+                  }
+                })
+              )
+            );
           }, Promise.reject);
         }
         function getTemplateDataFileGlob(original) {
           var suffix = this.config.jsDataFileSuffix;
           return original.apply(this, arguments).then(function(arr) {
-            return arr.concat([path.join(path.dirname(arr[0]), `*${suffix}.yaml`)]);
+            return arr.concat([
+              path.join(path.dirname(arr[0]), `*${suffix}.yaml`)
+            ]);
           }, Promise.reject);
         }
         monkeypatch(TemplateData, getLocalData);
